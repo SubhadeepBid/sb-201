40ca929652953a116f6dfb9a134aeda0d366b4fb
subha <subha@Jay_Jagannath>
2023-12-01 20:25:41 +0530

    Day-06 files added

---
+++ SB-201_Codes/SB-201_Day-06/Day_06_01_has-a-relationship-app-01/src/main/java/com/masai/model/Employee.java
@@ -0,0 +1,72 @@
+package com.masai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "emp_id_gen")
+	@SequenceGenerator(name = "emp_id_gen", sequenceName = "egen", allocationSize = 1, initialValue = 1000)
+	private int eid;
+	
+	private String ename;
+	
+	private int salary;
+	
+	private Address address;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int eid, String ename, int salary, Address address) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.salary = salary;
+		this.address = address;
+	}
+
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + ", address=" + address + "]";
+	}
+
+}
